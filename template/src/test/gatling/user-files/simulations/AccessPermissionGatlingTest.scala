dc69871bcb3863423c397d829cf95fefdd6cfd9b
luolei <askluolei@gmail.com>
2018-02-11 18:01:24 +0800

    gatling 测试

---
+++ template/src/test/gatling/user-files/simulations/AccessPermissionGatlingTest.scala
@@ -0,0 +1,76 @@
+import _root_.io.gatling.core.scenario.Simulation
+import ch.qos.logback.classic.{Level, LoggerContext}
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.duration._
+
+class AccessPermissionGatlingTest extends Simulation {
+
+    val context: LoggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+    // Log all HTTP requests
+    //context.getLogger("io.gatling.http").setLevel(Level.valueOf("TRACE"))
+    // Log failed HTTP requests
+    //context.getLogger("io.gatling.http").setLevel(Level.valueOf("DEBUG"))
+
+    // 定义baseURL
+    val baseURL = Option(System.getProperty("baseURL")) getOrElse """http://localhost:8080"""
+
+    val httpConf = http
+        .baseURL(baseURL)
+        .inferHtmlResources()
+        .acceptHeader("*/*")
+        .acceptEncodingHeader("gzip, deflate")
+//        .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+        .connectionHeader("keep-alive")
+        .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0")
+
+    val headers_http = Map(
+        "Accept" -> """application/json"""
+    )
+
+    val headers_http_authentication = Map(
+        "Content-Type" -> """application/json""",
+        "Accept" -> """application/json"""
+    )
+
+    val headers_http_authenticated = Map(
+        "Accept" -> """application/json""",
+        "Authorization" -> "${access_token}"
+    )
+
+    //具体的测试请求定义 看方法名就知道是干啥的了 后面需要修改的就是数据
+    var scn = scenario("Test the AccessPermission Controller")
+        .exec(http("First unauthenticated request")
+        .get("/api/account")
+        .headers(headers_http)
+        .check(status.is(401))).exitHereIfFailed
+        .pause(10)
+        .exec(http("Authentication")
+        .post("/api/authenticate")
+        .headers(headers_http_authentication)
+        .body(StringBody("""{"username":"admin", "password":"admin"}""")).asJSON
+        .check(header.get("Authorization").saveAs("access_token"))).exitHereIfFailed
+        .pause(1)
+        .exec(http("Authenticated request")
+        .get("/api/account")
+        .headers(headers_http_authenticated)
+        .check(status.is(200)))
+        .pause(10)
+        .repeat(2) {
+            exec(http("get all access permission")
+            .get("/api/access-permission")
+            .headers(headers_http_authenticated)
+            .check(status.is(200)))
+            .pause(10 seconds, 20 seconds)
+        }
+
+    //指定执行的脚本   返回的是用户
+    val users = scenario("Users").exec(scn)
+
+    //添加一些用户
+    setUp(
+        users.inject(rampUsers(Integer.getInteger("users", 100)) over (Integer.getInteger("ramp", 1) minutes))
+    ).protocols(httpConf)
+}
